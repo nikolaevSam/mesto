7eb1ceeea95d415651d77570227ff220f4422d5f
Nikolay <E2003640@nventco.com>
2022-11-23 18:04:18 +0500

    js: btnLike add/remove

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,49 @@
-let elements = document.querySelector('.elements');
+let btnLike = document.querySelectorAll('.element__btn-like');
 
-let element = elements.querySelector('.element');
+for (let i = 0; i < btnLike.length; i++) {
 
-let btnLike = element.querySelector('.element__like');
+    function addLike() {
 
+        if (btnLike[i].classList.contains('element__btn-like_inactive') === true) {
 
-btnLike.onclick = function(event) {
+            btnLike[i].classList.remove('element__btn-like_inactive');
 
-    btnLike.classList.add('element__like_active');
+            btnLike[i].classList.add('element__btn-like_active');
+
+        } else {
+
+            btnLike[i].classList.add('element__btn-like_inactive');
+
+            btnLike[i].classList.remove('element__btn-like_active');
+
+        }
+
+    }
+
+    btnLike[i].addEventListener('click', addLike);
 
 }
+
+// Находим форму в DOM
+// let formElement = // Воспользуйтесь методом querySelector()
+// Находим поля формы в DOM
+// let nameInput = // Воспользуйтесь инструментом .querySelector()
+// let jobInput = // Воспользуйтесь инструментом .querySelector()
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+// function formSubmitHandler (evt) {
+//     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+// }
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+// formElement.addEventListener('submit', formSubmitHandler); 
