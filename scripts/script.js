8ec8d8627c18b763def83510173feb4e83fde5fa
Nikolay <E2003640@nventco.com>
2022-11-25 13:00:40 +0500

    mesto: issue for review

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,7 @@ let btnLike = document.querySelectorAll('.element__btn-like');
 
 for (let i = 0; i < btnLike.length; i++) {
 
-    let addLike = () => {
-
-        (btnLike[i].classList.contains('element__btn-like_inactive') === true) ?
-
-        (btnLike[i].classList.remove('element__btn-like_inactive'), btnLike[i].classList.add('element__btn-like_active')) :
-
-        (btnLike[i].classList.add('element__btn-like_inactive'), btnLike[i].classList.remove('element__btn-like_active'))
-
-    }
+    let addLike = () => { (btnLike[i].matches(':hover') === false) ? btnLike[i].classList.toggle('element__btn-like_active') : btnLike[i].classList.toggle('element__btn-like_active') }
 
     btnLike[i].addEventListener('click', addLike);
 
@@ -30,13 +22,15 @@ let btnSave = document.querySelector('.form__btn-save');
 
 let openPopup = () => { (btnEdit.matches(':hover') === true) ? popup.classList.add('popup_opened') : false };
 
-let closePopup = () => { ((btnClose.matches(':hover') === true) || (btnSave.matches(':hover') === true)) ? popup.classList.remove('popup_opened') : false };
+let closePopup = (evt) => { ((btnClose.matches(':hover') === true) || (btnSave.matches(':hover') === true) || ((evt.code === 'Enter'))) ? popup.classList.remove('popup_opened') : false };
 
 btnEdit.addEventListener('click', openPopup);
 
 btnClose.addEventListener('click', closePopup);
 
 btnSave.addEventListener('click', closePopup);
+
+document.addEventListener( 'keyup', closePopup);
 
 // заполнение профиля из формы
 
@@ -50,6 +44,10 @@ let nameForm = document.querySelector('.profile-info__title');
 
 let jobForm = document.querySelector('.profile-info__subtitle');
 
+nameInput.value = nameForm.textContent;
+
+jobInput.value = jobForm.textContent;
+
 function formSubmitHandler(evt) {
 
     evt.preventDefault();
@@ -59,14 +57,4 @@ function formSubmitHandler(evt) {
 
 btnSave.addEventListener('click', formSubmitHandler);
 
-
-
-// let addLike = () => {
-
-//     (btnLike[i].classList.contains('element__btn-like_inactive') === true) ?
-
-//     (btnLike[i].classList.remove('element__btn-like_inactive'), btnLike[i].classList.add('element__btn-like_active')) :
-
-//     (btnLike[i].classList.add('element__btn-like_inactive'), btnLike[i].classList.remove('element__btn-like_active'))
-    
-// }
+document.addEventListener( 'keyup', formSubmitHandler);
