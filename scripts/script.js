7a2fb2d0ee38a78ea4beef02b88831366d7992b7
Nikolay <E2003640@nventco.com>
2022-11-27 17:47:05 +0500

    mesto: bug fix from reviewer

--- scripts/script.js
+++ scripts/script.js
@@ -1,41 +1,37 @@
 const popup = document.querySelector('.popup');
-
 const buttonEdit = document.querySelector('.profile-info__edit');
-
 const buttonSave = document.querySelector('.form__save');
-
 const buttonClose = popup.querySelector('.popup__close');
-
 const formElement = document.querySelector('.form');
-
 let nameForm = document.querySelector('.profile-info__title');
-
 let jobForm = document.querySelector('.profile-info__subtitle');
-
-let nameInput = formElement.querySelector('.form__name');
-
-let jobInput = formElement.querySelector('.form__job');
-
-nameInput.value = nameForm.textContent;
-
-jobInput.value = jobForm.textContent;
+let nameInput = formElement.querySelector('.form__input_data_name');
+let jobInput = formElement.querySelector('.form__input_data_job');
 
 
-let openPopup = () => { (buttonEdit.matches(':hover') === true) ? popup.classList.add('popup_opened') : false };
+let openPopup = () => {
+  nameInput.value = nameForm.textContent;
+  jobInput.value = jobForm.textContent;
 
-let closePopup = (event) => { ((buttonClose.matches(':hover') === true) || (buttonSave.matches(':hover') === true)) ? popup.classList.remove('popup_opened') : false };
+  (buttonEdit.matches(':hover') === true) ?
+    popup.classList.add('popup_opened')
+    : false
+};
+
+let closePopup = (event) => {
+  ((buttonClose.matches(':hover') === true) || (buttonSave.matches(':hover') === true)) ?
+    popup.classList.remove('popup_opened')
+    : false
+};
 
 function formSubmitHandler(evt) {
+  evt.preventDefault();
 
-    evt.preventDefault();
-
-    ((nameInput.value === '') || (jobInput.value === '')) ? alert('Please enter your name and job!') : (nameForm.textContent = nameInput.value, jobForm.textContent = jobInput.value)
+  nameInput.value === "" || jobInput.value === "" ?
+    alert('Please enter your name and job!') :
+    (nameForm.textContent = nameInput.value, jobForm.textContent = jobInput.value)
 }
 
-buttonSave.addEventListener('click', formSubmitHandler);
-
+buttonSave.addEventListener('submit', formSubmitHandler);
 buttonEdit.addEventListener('click', openPopup);
-
 buttonClose.addEventListener('click', closePopup);
-
-buttonSave.addEventListener('click', closePopup);
