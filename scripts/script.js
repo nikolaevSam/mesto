8cf88486d0c865d96f03dc45f2df2393e6969b5e
Nikolay <E2003640@nventco.com>
2022-11-28 16:14:20 +0500

    mesto: final issue ihope

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,35 @@
 const popup = document.querySelector('.popup');
-const popupContainer = popup.querySelector('.popup__container');
 const buttonEdit = document.querySelector('.profile-info__edit');
-const buttonSave = document.querySelector('.form__save');
 const buttonClose = popup.querySelector('.popup__close');
 const formElement = document.querySelector('.form');
-let nameInput = formElement.querySelector('.form__input_data_name');
-let jobInput = formElement.querySelector('.form__input_data_job');
+let nameProfile = formElement.querySelector('.form__input_data_name');
+let jobProfile = formElement.querySelector('.form__input_data_job');
 let nameForm = document.querySelector('.profile-info__title');
 let jobForm = document.querySelector('.profile-info__subtitle');
 
 let openPopup = () => {
-  nameInput.value = nameForm.textContent;
-  jobInput.value = jobForm.textContent;
+  nameProfile.value = nameForm.textContent;
+  jobProfile.value = jobForm.textContent;
   popup.classList.add('popup_opened');
 };
-buttonEdit.addEventListener('click', openPopup);
 
 let closePopup = () => {
   popup.classList.remove('popup_opened');
 };
-buttonClose.addEventListener('click', closePopup);
 
 function formSubmitHandler(evt) {
   evt.preventDefault(evt);
-  nameForm.textContent = nameInput.value;
-  jobForm.textContent = jobInput.value;
+  nameForm.textContent = nameProfile.value;
+  jobForm.textContent = jobProfile.value;
   closePopup();
 };
-formElement.addEventListener('submit', formSubmitHandler);
-
 
 popup.addEventListener('click', function (evt) {
   (evt.target === evt.currentTarget) ?
     closePopup()
     : false
 });
+
+buttonEdit.addEventListener('click', openPopup);
+buttonClose.addEventListener('click', closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
