8e92bee6545c699863775caa539cb7872f9f749c
Nikolay <E2003640@nventco.com>
2022-11-28 10:38:48 +0500

    mesto: issue for review

--- scripts/script.js
+++ scripts/script.js
@@ -1,37 +1,38 @@
 const popup = document.querySelector('.popup');
+const popupContainer = popup.querySelector('.popup__container');
 const buttonEdit = document.querySelector('.profile-info__edit');
 const buttonSave = document.querySelector('.form__save');
 const buttonClose = popup.querySelector('.popup__close');
 const formElement = document.querySelector('.form');
-let nameForm = document.querySelector('.profile-info__title');
-let jobForm = document.querySelector('.profile-info__subtitle');
 let nameInput = formElement.querySelector('.form__input_data_name');
 let jobInput = formElement.querySelector('.form__input_data_job');
+let nameForm = document.querySelector('.profile-info__title');
+let jobForm = document.querySelector('.profile-info__subtitle');
 
 
 let openPopup = () => {
   nameInput.value = nameForm.textContent;
   jobInput.value = jobForm.textContent;
-
-  (buttonEdit.matches(':hover') === true) ?
-    popup.classList.add('popup_opened')
-    : false
+  popup.classList.add('popup_opened');
 };
+buttonEdit.addEventListener('click', openPopup);
 
-let closePopup = (event) => {
-  ((buttonClose.matches(':hover') === true) || (buttonSave.matches(':hover') === true)) ?
-    popup.classList.remove('popup_opened')
-    : false
+let closePopup = () => {
+  popup.classList.remove('popup_opened');
 };
+buttonClose.addEventListener('click', closePopup);
 
 function formSubmitHandler(evt) {
-  evt.preventDefault();
+  evt.preventDefault(evt);
+  nameForm.textContent = nameInput.value;
+  jobForm.textContent = jobInput.value;
+  closePopup();
+};
+formElement.addEventListener('submit', formSubmitHandler);
 
-  nameInput.value === "" || jobInput.value === "" ?
-    alert('Please enter your name and job!') :
-    (nameForm.textContent = nameInput.value, jobForm.textContent = jobInput.value)
-}
 
-buttonSave.addEventListener('submit', formSubmitHandler);
-buttonEdit.addEventListener('click', openPopup);
-buttonClose.addEventListener('click', closePopup);
+popup.addEventListener('click', function (evt) {
+  (evt.target === evt.currentTarget) ?
+    closePopup()
+    : false
+});
