c14138693df5498ae5b546c3715416eb7eae6b5e
Nikolay <E2003640@nventco.com>
2022-11-30 18:08:31 +0500

    JS: add place form

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,41 @@
 const popup = document.querySelector('.popup');
-const buttonClose = document.querySelector('.popup__close');
+const popup_profile = document.getElementById('popup_profile');
+const popup_element = document.getElementById('popup_element');
 const buttonEdit = document.querySelector('.profile-info__edit');
 const formElement = document.querySelector('.form');
+let buttonAdd = document.querySelector('.profile__add');
+let buttonClose = document.querySelectorAll('.popup__close');
 let nameInput = formElement.querySelector('.form__input_data_name');
 let jobInput = formElement.querySelector('.form__input_data_job');
 let nameProfile = document.querySelector('.profile-info__title');
 let jobProfile = document.querySelector('.profile-info__subtitle');
+let elementContainer = document.querySelector('.elements');
+let buttonLike = elementContainer.querySelectorAll('.element__like');
 
-let openPopup = () => {
+let openPopupProfile = () => {
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
-  popup.classList.add('popup_opened');
+  popup_profile.classList.add('popup_opened');
+};
+
+let openPopupElement = () => {
+  popup_element.classList.add('popup_opened');
 };
 
 let closePopup = () => {
   popup.classList.remove('popup_opened');
+}
+
+for (let i = 0; i < buttonClose.length; i++) {
+  buttonClose[i].addEventListener('click', closePopup);
 };
+
+for (let i = 0; i < buttonLike.length; i++) {
+  let addLike = () => {
+      buttonLike[i].classList.toggle('element__like_active')
+  };
+  buttonLike[i].addEventListener('click', addLike);
+};;
 
 function formSubmitHandler(evt) {
   evt.preventDefault(evt);
@@ -29,6 +49,6 @@ popup.addEventListener('click', function (event) {
     closePopup() : false;
 });
 
-buttonEdit.addEventListener('click', openPopup);
-buttonClose.addEventListener('click', closePopup);
+buttonEdit.addEventListener('click', openPopupProfile);
+buttonAdd.addEventListener('click', openPopupElement);
 formElement.addEventListener('submit', formSubmitHandler);
