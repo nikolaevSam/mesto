7e83540eaedb9bcc138abf5c0d031ccd5d6157e8
Nikolay <E2003640@nventco.com>
2022-12-05 18:19:52 +0500

    JS: develop

--- scripts/script.js
+++ scripts/script.js
@@ -1,131 +1,90 @@
-const popup_profile = document.getElementById('popup_profile');
-const popup_element = document.getElementById('popup_element');
+const buttonAdd = document.querySelector('.profile__add');
 const buttonEdit = document.querySelector('.profile-info__edit');
-const formElement = document.querySelector('.form');
-let buttonAdd = document.querySelector('.profile__add');
-let buttonClose = document.querySelectorAll('.popup__close');
-let nameInput = formElement.querySelector('.form__input_data_name');
-let jobInput = formElement.querySelector('.form__input_data_job');
+const buttonClose = document.querySelectorAll('.popup__close');
+const buttonDeletePlace = document.querySelectorAll('.element__delete');
+const buttonLike = document.querySelectorAll('.element__like');
+let elementPlace = document.getElementById('popup_element');
+let elementProfile = document.getElementById('popup_profile');
+let formPlace = elementPlace.querySelector('.form');
+let formProfile = elementProfile.querySelector('.form');
+let inputPlace = elementPlace.querySelector('.form__input_data_place');
+let inputUrl = elementPlace.querySelector('.form__input_data_url');
+let nameInput = formProfile.querySelector('.form__input_data_name');
+let jobInput = formProfile.querySelector('.form__input_data_job');
 let nameProfile = document.querySelector('.profile-info__title');
 let jobProfile = document.querySelector('.profile-info__subtitle');
-let popups = document.querySelectorAll('.popup');
-let elementContainer = document.querySelector('.elements');
-let buttonSave = document.getElementById('form_element');
-let placeInput = document.querySelector('.form__input_data_place');
-let urlInput = document.querySelector('.form__input_data_url');
-const initialCards = [
-  {
-      name: 'Архыз',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
-  },
-  {
-      name: 'Челябинская область',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
-  },
-  {
-      name: 'Иваново',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
-  },
-  {
-      name: 'Камчатка',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
-  },
-  {
-      name: 'Холмогорский район',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
-  },
-  {
-      name: 'Байкал',
-      link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
-  }
-];
-function addCards() {
-  for (let i = 0; i < initialCards.length; i++) {
-      elementContainer.insertAdjacentHTML('beforeend', `
-  <article class="element">
-  <button class="element__delete" aria-label="удалить место" type="button"></button>
-  <img src="${initialCards[i].link}" alt="${initialCards[i].name}" class="element__image">
-  <h2 class="element__text">${initialCards[i].name}</h2>
-  <button class="element__like" type="button"></button>
-  </article>
-  `);
-  };
-};
-addCards();
-let buttonLike = elementContainer.querySelectorAll('.element__like');
-let elements = elementContainer.querySelectorAll('.element');
-let buttonDeletePlace = elementContainer.querySelectorAll('.element__delete');
+let popupAll = document.querySelectorAll('.popup');
+let elementAll = document.querySelectorAll('.element');
 
-let openPopupProfile = () => {
-  nameInput.value = nameProfile.textContent;
-  jobInput.value = jobProfile.textContent;
-  popup_profile.classList.add('popup_opened');
+let addCard = (evt) => {
+  evt.preventDefault(evt);
+  const elementTemplate = document.querySelector('#element-template').content;
+  let element = elementTemplate.querySelector('.element').cloneNode(true);
+  element.querySelector('.element__text').textContent = inputPlace.value;
+  element.querySelector('.element__image').src = inputUrl.value;
+  element.querySelector('.element__image').alt = inputPlace.value;
+  elementContainer.prepend(element);
+  closePopup();
 };
 
-let openPopupElement = () => {
-  popup_element.classList.add('popup_opened');
-};
-
-let closePopup = () => {
-  for (let i = 0; i < popups.length; i++) {
-    popups[i].classList.remove('popup_opened');
-  };
-};
-
-for (let i = 0; i < popups.length; i++) {
-  buttonClose[i].addEventListener('click', closePopup);
-};
-
-function formSubmitHandler(evt) {
+let formSubmitHandler = (evt) => {
   evt.preventDefault(evt);
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
   closePopup();
 };
 
-for (let i = 0; i < popups.length; i++) {
-popups[i].addEventListener('click', function (event) {
-  event.target === event.currentTarget ?
-    closePopup() : false;
-});
+let openPopup = (popupName) => {
+  popupName.classList.add('popup_opened');
 };
 
-// const likeButtons = page.querySelectorAll(“.element__like”);
-// function like(clickLike) {
-//   clickLike.classList.toggle(“element__like_active”);
-// }
-// for (let i = 0; i < likeButtons.length; i++) {
-//   likeButtons[i].addEventListener(“click”, () => like(likeButtons[i]));
-// }
+let closePopup = () => {
+  for (let i = 0; i < popupAll.length; i++) {
+    popupAll[i].classList.remove('popup_opened');
+  };
+};
+
+for (let i = 0; i < popupAll.length; i++) {
+  buttonClose[i].addEventListener('click', closePopup);
+};
 
 for (let i = 0; i < buttonLike.length; i++) {
   let addLike = () => {
-      buttonLike[i].classList.toggle('element__like_active')
+    buttonLike[i].classList.toggle('element__like_active')
   };
   buttonLike[i].addEventListener('click', addLike);
 };
 
-for (let i = 0; i < elements.length; i++) {
+for (let i = 0; i < elementAll.length; i++) {
   let deletePlace = () => {
-      elements[i].remove();
+    elementAll[i].remove();
   };
   buttonDeletePlace[i].addEventListener('click', deletePlace);
 };
 
-let addCard = (evt) => {
-  evt.preventDefault(evt);
-  elementContainer.insertAdjacentHTML('afterBegin', `
-  <article class="element">
-  <button class="element__delete" aria-label="удалить место" type="button"></button>
-  <img src="${urlInput.value}" alt="${placeInput.value}" class="element__image">
-  <h2 class="element__text">${placeInput.value}</h2>
-  <button class="element__like" type="button"></button>
-  </article>
-  `);
-  closePopup();
+for (let i = 0; i < popupAll.length; i++) {
+  popupAll[i].addEventListener('click', (event) => {
+    event.target === event.currentTarget ?
+      closePopup() : false;
+  });
 };
 
-buttonSave.addEventListener('click', addCard);
-buttonEdit.addEventListener('click', openPopupProfile);
-buttonAdd.addEventListener('click', openPopupElement);
-formElement.addEventListener('submit', formSubmitHandler);
+formProfile.addEventListener('submit', formSubmitHandler);
+formPlace.addEventListener('submit', addCard);
+buttonAdd.addEventListener('click', () => openPopup(popup_element));
+buttonEdit.addEventListener('click', () => {
+  openPopup(popup_profile)
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+});
+
+let elementImage = document.querySelectorAll('.element__image');
+ let zoom = () => {
+  for (let i = 0; i < elementImage.length; i++) {
+
+  }
+ }
+document.querySelector('.element__image').addEventListener('click', (evt) => {
+  console.log(evt.target.src);
+  Console.log(evt.target.alt);
+});
